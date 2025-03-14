997b9e201f6e667850f469b98910a4b8ba213cf5
youpingh <34691387+youpingh@users.noreply.github.com>
2025-03-14 13:14:21 -0600

    The project init

---
+++ htmls/all-words-more.js
@@ -0,0 +1,112 @@
+/**
+ * Add this JSON array is used to store all the words and it is read by an init function to load them up for the App.
+ * This is populated in a page file: all-words.
+ */
+[
+	{
+		"category": "Number",
+		"cname": "数字",
+		"words": [
+			{ "chinese": "千", "english": "Thousand", "pinyin": "Qiān", "phrase": ["千万", "五千", "千家万户"], "sentence": "十个一百就是一千。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPtYcYv4Fqb7fffa-5WQ9xkxvMI6vbbSk1RtpSv236PdXS1827mWVqrC2CzIgHHHZe2BxOleOKaQZVUJJVLGv2sMdf8wGRN-Hwe3hcaKeKKWU2EW_JePrvQ5WCfCQ8FByWPXD0T7MlSLLnATgcKtOWC-w=w1024-h1024-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Shape",
+		"cname": "形状",
+		"words": [
+			{ "chinese": "面", "english": "Surface", "pinyin": "Miàn", "phrase": ["表面", "面向", "面红耳赤"], "sentence": "纸上是一面浅蓝色的小旗子。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOAfijaa_m4dvJiXvjtn4jCWZ-zCDZ4tPrLOKS9I90DslMMhj1MgCPV45zSvmgxF9VmONqcQzmC4b5hygsa6suwH0XS4gXy8FcWALQRVqIuFbWaihB0KxHFHX1R3NP2IZvC0IppYToinOdPgPY72auClw=w1355-h1355-s-no-gm?authuser=0" },
+		]
+	},
+	{
+		"category": "Body",
+		"cname": "身体",
+		"words": [
+			{ "chinese": "人", "english": "Human", "pinyin": "Rén", "phrase": ["男人", "女人", "人人平等"], "sentence": "屋里有好几个人，正在聊天。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOUhCmC5AKFGyY_67o7t9BC3vbCsEBazcw936AsXjOaXUnPK_q8a1_5Lg5VH3RmvG871hHyMQAbuydCnFyRnxVr43QVZSCUyJ0ZsT5gY6qxCnEftnyQdF60K01_50bFIw1xJ0K-CZRjm2lCKDYZi5T3-A=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "胸", "english": "Chest", "pinyin": "Xiōng", "phrase": ["胸口", "胸脯", "胸有成竹"], "sentence": "看见狼，他很害怕，胸口突突直跳。", "image": "https://lh3.googleusercontent.com/pw/AP1GczNuMyg_gj2nI49Ckj8KbQzchIynIzYP3FrygyFVhxZa5z7Co2qE5bSYGS2eoqhZvqwPB0AeOpc_mGZ5mYtwiQNLRgQ1EhU4CF1jVVH6eVrJsR3nBlS_E8qtHe5ZssAzThzXh82PgmfsONppiwLlMQudCA=w1024-h1024-s-no-gm?authuser=0" },
+		]
+	},
+	{
+		"category": "Nature",
+		"cname": "自然",
+		"words": [
+			{ "chinese": "气", "english": "Air", "pinyin": "Qì", "phrase": ["生气", "天气", "气喘吁吁"], "sentence": "明天的天气怎么样，会下雨吗？", "image": "https://lh3.googleusercontent.com/pw/AP1GczPsjT9h8MChu-jgmROrGE_w-lk7FVvZ-At2lEyDFzFhtb3gKMY1fIyY6Zx3bHSNRBdtYP8j4T2hSYQgKTXf7CUCPDCpd_MVaJYhZB8yZOz7s7NbWuLyMVHgyDkAwS3yWt8WPUkEeC6jtaGeNC30-016Zw=w1292-h1292-s-no-gm?authuser=0" },
+		]
+	},
+	{
+		"category": "Furniture",
+		"cname": "家具",
+		"words": [
+			{ "chinese": "房", "english": "House", "pinyin": "Fáng", "phrase": ["房子", "楼房", "房前屋后"], "sentence": "在我们家的房前屋后都种得有花草。", "image": "https://lh3.googleusercontent.com/pw/AP1GczN7MPXFD8BO7fn_HYTJBNlKVA35mUheeR2xyIpaRS4nqI1QaT-vKyY32ULgjCIVPFa4sPz8brVamqlbXw4fewdlAyUnw0XP3CTRcETDrvA-gEeAX_VFcY0_DqJalHmYfVKatDL_ThAn7955S5EUcpuHDQ=w1316-h1316-s-no-gm?authuser=0" },
+			{ "chinese": "沙发", "english": "Sofa", "pinyin": "Shāfā", "phrase": ["沙发", "沙发床"], "sentence": "客厅里，在电视机前摆着皮沙发。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPgBBU2yJBCMLKjGha-zOas6vJfQqE7swSxqVwb6oj0cj3OjkinLeqJgFRqzxf4i5-5uRf1RkWc4vc0nvJJmtEBZRPnfcT130SDud3WpXvOg9yeutN7yXg9BzGcukb1kC4odeXFMZSaVHPiw0kQ0_5izw=w1316-h1316-s-no-gm?authuser=0" },
+			{ "chinese": "楼梯", "english": "Stairs", "pinyin": "Lóutī", "phrase": ["楼梯"], "sentence": "上下楼都要走楼梯。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPLBI7D_RbSX97CRongjfaItwt1ZQ8BFUfi3OMvcRhj_amB_FD1ZYXrSeWxKy_wFGD7gOYJqYHw0Y7Wb7DL0NAs6akuCwkLz_LC0yP38UNqOWXrIb6Ji_vIGHQamF1oKdfCr4Eq3qRN8PKq4JWkBXoFEg=w1316-h1316-s-no-gm?authuser=0" },
+			{ "chinese": "电视", "english": "TV", "pinyin": "Diànshì", "phrase": ["高清电视", "彩色电视"], "sentence": "每天不要看太多电视，对眼睛不好。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPwhFXzwoJRiGXUoBt__eWmBSPl8L34OBVA4HrX8gtty-rb6gP6WwOgBp9oAWWZwf27IiqC9GVZKFKcphrahmQ0IUjzVh_GTyJj2apdIVDKqpkusRAkpIGg4ugYo1zssTUY6B2qCVcv0JGqL1qAIcC_YA=w1316-h1316-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Mammal",
+		"cname": "哺乳动物",
+		"words": [
+			{ "chinese": "狮", "english": "Lion", "pinyin": "Shī", "phrase": ["狮子", "雄狮", "狮子大开口"], "sentence": "非洲的草原上有很多狮子。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMngQFMPovR0thG7lmfbgHS6J0haCZ_Z3hRGquyJnP0KzepQvRZdzp4DMt2jKW0OCZJfGXArITHPyD5Q9dnYz3BdZNvpBkCowiYWRNxTCNx7Qgcfl5L2Cvtv9CCipIgZdSBCEhFn5yjjv1l5Sdwubv6Ow=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "熊", "english": "Bear", "pinyin": "Xióng", "phrase": ["黑熊", "北极熊", "虎背熊腰"], "sentence": "北极熊生活在北极的冰面上。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMwbgacindGyks3CT082kehcnXP17-tbislGnt0RjVD-W5xdmqICr0mBQmwewLl2x8Av0TXkEZNU0znOojwNiPqo8e2sUF8zy-a1RVD_-7V4a0XAwCoBUhPJxH4pGaTbBcBHlqJHxq2x_vH06nZqmz7bg=w1346-h1346-s-no-gm?authuser=0" },
+			{ "chinese": "蛇", "english": "Snake", "pinyin": "Shé", "phrase": ["蟒蛇", "蛇行", "虎头蛇尾"], "sentence": "有的蛇有毒，有的没有。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPoLqO8MtoXEvZtaglhKywjaD8k72NJvSkSTM8alDctN90GyfiZulSkLQUhu7PStSgkRC7Qjl_Y5hkNZg9IpSmi_3bn_R2zv14lES9IUAFv_4RYcsRJ-1UhrSDOoIEF3qMPT90TKO3Je32jYfyWIodbbg=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "斑马", "english": "Zebra", "pinyin": "bān mǎ", "phrase": ["斑马线"], "sentence": "斑马线就是人行横道线。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMniXtUf4It6JFDTAxQs61plbCnR7ClGz8NmDInZaeC9N3tjI_ldPVzcrktneYIYFwEoII-YboOunPrpzLhSAkeBWi_YLPYfFuRu9fR0D-UkvFOO44XEsHhoQ-WeQ_IHLYNUonVX0CBc5KfAwQfKJgq1g=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "骆驼", "english": "Camal", "pinyin": "luò tuó", "phrase": ["骆驼"], "sentence": "羊群里跑出一头骆驼来。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPlVuDN0_v7l7VI4uJCkuxn3W2heAjJNMMMI9sqW_k2a7Ukk0gI-TT-lD_P1lf4STvUYlKnYxZTGGkYwnwgCvmoHfvoX8VJAkUzUrfibLnmg7a3DvHbiDn-d3O4XxS4E4mjl1JzfISunhCP-TxkkgGl6A=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "长颈鹿", "english": "Giraffe", "pinyin": "cháng jǐng lù", "phrase": ["长颈鹿"], "sentence": "长颈鹿的脖子很长，所以可以吃到大树上面的叶子。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMhlcLxPHzr7Lg8V272K0TXXJ8-ECEFNXMrCPcAyy9sIaUH9BaXFgCzcB0Q0Pqngx1sfooM3AdYVZicP2oJsEcWWU0Gybjr_jjPpmA7LgjkFsDpuks8iZO0CH4YPAdBDqwB9iYhFXcSFAz1J_ufmKYqsA=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "独角兽", "english": "Unicorn", "pinyin": "Dú jiǎo shòu", "phrase": ["独角兽"], "sentence": "独角兽长得像马，头上有一只角。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPw-GIUVSM9BVbQtP9a8SVVwBUBOD3atIBoUe_qZ2aPz8dEP1O4jK5YWuAbpA8QKo4SZo7NAwjWxUXQw7G5Fbx2rOrQuIkY7fDgjzaSL2MJaEBIm625asdLRsoMBd5OCCRwRKEATE9lbvrs5idX1N7dBg=w1337-h1337-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Insect",
+		"cname": "昆虫",
+		"words": [
+			{ "chinese": "蚂蚱", "english": "Cricket", "pinyin": "mà zhà", "phrase": ["一条线上拴的蚂蚱"], "sentence": "蚂蚱也叫蝗虫。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMBHj_3LEdBBf0zIYhj6n0pUB-MQMnLq5Kf--o7OsKJ_MOa0E7i03G8R2vkWwuwP7gp-6sRMxvbHCcvfpmvyb3_hcZFXTzamKSD9dH0RrLxHtbRT4PTUck4_RL0HYdRetEAYhuHSn8mAHfK-elncWSZdQ=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "蜻蜓", "english": "Dragonfly", "pinyin": "Qīngtíng", "phrase": ["蜻蜓", "蜻蜓点水"], "sentence": "夏天的傍晚，在屋外可以看见蜻蜓，也有蚊子。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOZEEmtnluyT122iMWqJstObJcoJFSrHH3PBpLow8LX_P3leJ6eEv09WfdC2B-lmgFvR5N31R8vquY5uLgK6iPbrhleQiuOtK3Zq_dC-XAkcnTUhStYFwkZQ3wiPigryXxFBJlR4akmrnqIblQ0ge661g=w1337-h1337-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Other Animal",
+		"cname": "其它动物",
+		"words": [
+			{ "chinese": "蜥蜴", "english": "Lizard", "pinyin": "Xīyì", "phrase": ["蜥蜴"], "sentence": "蜥蜴还有一个名字叫四脚蛇。", "image": "https://lh3.googleusercontent.com/pw/AP1GczNiLTRf9DVSU7gxLATgvfhONyIDvlcoZf49UzkJCvRVqcK1l4PyiQB6YqytdG7g10Us_RQy5X7G-jnRQU3s9Us6vRolxD_AnTihtwRFnPQ1Fm_dcbmI85bfpn9OsfUhnhRSGSxbX2XA2xQvszPtYoOHJw=w1337-h1337-s-no-gm?authuser=0" },
+			{ "chinese": "娃娃鱼", "english": "Giant Salamander", "pinyin": "Wáwáyú", "phrase": ["娃娃鱼"], "sentence": "娃娃鱼是一种两栖动物。", "image": "https://lh3.googleusercontent.com/pw/AP1GczN-huw7Ykk5qZuDZgf-8ljHtrBn2DEEmaY6lkvyZbp-H3Iat0rP5TyX2f0uN6uYIFAaPPBq43m3LBNbJO4vtxTWW0H1KZZLl68C147EEVtr_ivYkFFLeW2djX-JKVn6zfu05OBriYq3GDFPSJUtIzTr7Q=w1337-h1337-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Direction",
+		"cname": "方向",
+		"words": [
+			{ "chinese": "东", "english": "Front", "pinyin": "Dōng", "phrase": ["东边", "东方", "各奔东西"], "sentence": "每天早上，太阳从东边升起来。", "image": "https://lh3.googleusercontent.com/pw/AP1GczORH7q3wwA4O5ynkambHIw3Db0Kz1YoBPnrcpbvEGzDMLb6UOLRCg3GVL_ozWlFZ7P2N_LJlZ9XePIqCq9bn1_RdYNg-HZ0OWmBo-N0xzLNBNhnN-O_JfRBnxPIXgMpkZasFmgzkwyDtUiwZdI6oa2iYQ=w1330-h1330-s-no-gm?authuser=0" },
+			{ "chinese": "西", "english": "Left", "pinyin": "Xī", "phrase": ["西边", "西方", "东拉西扯"], "sentence": "每天傍晚，太阳从西边落下去。", "image": "https://lh3.googleusercontent.com/pw/AP1GczN3R5EwtrsxfRDW6pnI-GTDLzCeklCiydgprjwhvoAQCBgbaKzjNNXcjFlqdhECecZD1t1pJk4KwWfcnLZlNOMbzHrjTRZf1QtN7jHTokfAAbEOMmZomo5LELZYylR2vT2pq2NX9rJ2R3jQAFa3fJ6qtw=w1330-h1330-s-no-gm?authuser=0" },
+			{ "chinese": "南", "english": "Back", "pinyin": "Nán", "phrase": ["南边", "南方", "走南闯北"], "sentence": "阳光可以从朝南的窗户照进来。", "image": "https://lh3.googleusercontent.com/pw/AP1GczNisgO41YvC0bqzdjWl-5IrtVQ_UP3P9SMpNIn57mqZg7QsSjqLGnMJLgjGkk7CrjUPuxoxhClbcDmMaFaiNlHvEiIQzH24mNHuZr4ycJzrFKtMsnmHqz44TczzV3KfMDb2sRnqW7KdGmKg2BKe3D6y0Q=w1330-h1330-s-no-gm?authuser=0" },
+			{ "chinese": "北", "english": "Right", "pinyin": "Běi", "phrase": ["北边", "北方", "天南地北"], "sentence": "北面的窗户没有阳光，屋里会很冷。", "image": "https://lh3.googleusercontent.com/pw/AP1GczNLGW5PaVjoz1voeCXBHyk8laxAEWqKnlAB-uzyYfwct5UbPyXZzbwEhcTAdn2-jwkFqYr6GMCQfj3l1jJOE0QFU4_MCiXvqMbQe43-vIEwEfvK8Z5HBZkoMwzrOoxDF52k2A6DSOtd0J_5TTqcIpp0xw=w1330-h1330-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Verb",
+		"cname": "动词",
+		"words": [
+			{ "chinese": "叫", "english": "Call", "pinyin": "Jiào", "phrase": ["叫声", "叫人", "叫苦不迭"], "sentence": "你叫弟弟来吃饭。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPOf2pNKjMKAQW1u51nl-XMq8S0OX9HHC5WQHRwmBme0e3Drd_yvS7m8v_cE-jllBTrbKBIpgD7KanVeLEzyD_LONJ9VQEiqC6H90uPFycxjYtA17lMuxX7iy9o0TUvhbv7wYQjOekfUjEzD3W5AO78fQ=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "吵", "english": "Quarrel", "pinyin": "Chǎo", "phrase": ["吵闹", "争吵", "吵吵闹闹"], "sentence": "有话慢慢说，别吵。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMYtbayU8WtDIU_YnpsN4ze5_Ymm67hMeP0OAhhLwJiefbkGqhjEK8NjVVzl1PEGuRMzOF4wh9zLTfRbQAfwcYw1McyQYeaXn3iOtapu4dxMAbTK7sSQyJbuj8Iy-DVuguMXwtcEj8Oly1h4u85OG-nvg=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "滚", "english": "Wallow", "pinyin": "Gǔn", "phrase": ["打滚", "滚雪球", "连滚带爬"], "sentence": "皮球一下子滚到床底下去了。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPaqeDxBaOlI9fHUe49cCq0qCWoL6bmKEcjEfCMzBQhREX1PS9IkCtrKiftNna_UTGbFDdH8Zj6m-5TZ93g--KUTxZwmdN91hUSrIq9UOv6o4D9x-TzKLnIPqRimUOZZPAiZS8S4TxT4nKBeMFMbZGmSQ=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "抓", "english": "Grasp", "pinyin": "Zhuā", "phrase": ["抓住", "抓周", "抓耳挠腮"], "sentence": "抓住了缰绳，别让狗乱跑。", "image": "https://lh3.googleusercontent.com/pw/AP1GczONRK0aemqvvmiNwOR0Kc8EPDxPStlfvshb3Wwh4OZ8p8YetI0agU3OfaoNLAA5lh7TzNlGC2sC0EcL2BfIIfILmpNsN81eiClh0kSoSViFUf_XX3JEzZ6UTvIrxAknVCRG-zkXWZBsgmiBcJ2saTXfjg=w1024-h1024-s-no-gm?authuser=0" }
+		]
+	},
+	{
+		"category": "Adj",
+		"cname": "形容词",
+		"words": [
+			{ "chinese": "弯", "english": "Bend", "pinyin": "Wān", "phrase": ["弯曲", "拐弯", "宁折不弯"], "sentence": "动物园里有一条弯弯的小河。", "image": "https://lh3.googleusercontent.com/pw/AP1GczNn9FZbVjrMdP-TPWzkbnr9wvoqMdVUeYlD27Eor2UnL5PRmE9TsojzQaS4Q3HCD1x4-Cyp0cW93Drjdrz34eoSzmr_sO1IZc0xdUFw_rtwt1rFIOLyyumZb5bBgZAlVs-I8zFfTgUxXCCFLoLqHfO7Xw=w767-h767-s-no-gm?authuser=0" },
+			{ "chinese": "直", "english": "Straight", "pinyin": "Zhí", "phrase": ["笔直", "直接", "心直口快"], "sentence": "院子里那棵树长得又高又直。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMZtkeK_qIOB9IfXwIWd9PN1CWz5w62dGrelzlHUr7g-ecJ6A-juStJasAwUI7WqDxImk7S4ekg3X25Bxvx0Ccp-XENLuuymh9HOPkOeR20A1ZWq-5nT_yAaUlOckl-oieyKfsZ1YBbBCJEtdi4XyLg3w=w760-h760-s-no-gm?authuser=0" },
+			{ "chinese": "乱", "english": "Messy", "pinyin": "Luàn", "phrase": ["脏乱", "心乱", "乱七八糟"], "sentence": "早上起床后，床上很乱，要收拾整齐。", "image": "https://lh3.googleusercontent.com/pw/AP1GczO9nXT-a8eaPfHc7glbePfVe8kO2vyVD66r1bMx4EZ-qcbR6PVYX3r7J5AmkR0wb_cuN6q8IUxbWT-LCmIUbCNfWSyX2z969wYqwJ5KX6gp28lZszec5HNff0i5e0bD76U4RsL4jLCpz60ILkG6LlP-3g=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "平", "english": "Flat", "pinyin": "Píng", "phrase": ["平常", "躺平", "平心静气"], "sentence": "去公园的路不平，走路要小心。", "image": "https://lh3.googleusercontent.com/pw/AP1GczPH6LRDVputHyUJN7y0DjPEin2s9MLR3nhlcd1czsG8q93YqQPnMxK7NQvGBM-kczSWPgkm3F0n6db3dVLlZ5IOuPpSOQzjvFbJSdC-At2MtYgei0o2FEaZlL_cEgCu5oklu6pWKus6bimcIi83n4h_hA=w601-h601-s-no-gm?authuser=0" },
+			{ "chinese": "远", "english": "Far", "pinyin": "Yuǎn", "phrase": ["远处", "不远", "远走高飞"], "sentence": "奶奶家挺远的，要去的话就得开车。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMCLw945DC8Ktu-XV1c43hoEvB9CtjTtEmABDuPCltToOYrZumHx_ppROysa71eHlOIi0rjs1QI_d-reWkrgQ8GW9GmRRNFaGgCiWv1LoUD7CVQrIfJadHGy-BtdeQ304-4wEEXiAiiYqSYobi6kBLtnA=w1026-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "近", "english": "Close", "pinyin": "Jìn", "phrase": ["走近", "近距离", "近在咫尺"], "sentence": "幼儿园离家很近，开车去几分钟就到了。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMCLw945DC8Ktu-XV1c43hoEvB9CtjTtEmABDuPCltToOYrZumHx_ppROysa71eHlOIi0rjs1QI_d-reWkrgQ8GW9GmRRNFaGgCiWv1LoUD7CVQrIfJadHGy-BtdeQ304-4wEEXiAiiYqSYobi6kBLtnA=w1026-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "真", "english": "True", "pinyin": "Zhēn", "phrase": ["真实", "真好", "真人真事"], "sentence": "今天的天气真好，咱们出去玩一会吧。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOkatyG2uNXt_sCJN3lhOZeSUOMhsjPiQqurOcnmyftHa0on0aC7sueDu3lp5n1WbBVVlRJgQvtyBiAXIcjEKVjs3ZQfZOS7UCtf2E9sGbiCewblWTV30tK6RM2ml3hS_eu7_XhEZpK-wBEdR5k0X6Ypg=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "假", "english": "False", "pinyin": "Jiǎ", "phrase": ["假的", "假装", "弄假成真"], "sentence": "这串葡萄是假的，不能吃。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOkatyG2uNXt_sCJN3lhOZeSUOMhsjPiQqurOcnmyftHa0on0aC7sueDu3lp5n1WbBVVlRJgQvtyBiAXIcjEKVjs3ZQfZOS7UCtf2E9sGbiCewblWTV30tK6RM2ml3hS_eu7_XhEZpK-wBEdR5k0X6Ypg=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "胖", "english": "Fat", "pinyin": "Pàng", "phrase": ["胖子", "胖小子", "高矮胖瘦"], "sentence": "咱们家的狗最近长胖了。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMTGZ5z1zVbMfUsEJO6WOfRlR0VJI69X25m0nkue8p5Hc1DRKnAar_s7n2iN9PIfJynerHeEUDmL3XO0l_0QXHkobQO3fqiV0lL2a49U6MEwl2waaWxYyl7It9i5-et56lo9zUvqgj8LPQArAm0Uh1chQ=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "瘦", "english": "Thin", "pinyin": "Shòu", "phrase": ["瘦肉", "肥瘦", "骨瘦如柴"], "sentence": "你看那只猫多瘦呀。", "image": "https://lh3.googleusercontent.com/pw/AP1GczMTGZ5z1zVbMfUsEJO6WOfRlR0VJI69X25m0nkue8p5Hc1DRKnAar_s7n2iN9PIfJynerHeEUDmL3XO0l_0QXHkobQO3fqiV0lL2a49U6MEwl2waaWxYyl7It9i5-et56lo9zUvqgj8LPQArAm0Uh1chQ=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "美", "english": "Beautiful", "pinyin": "Měi", "phrase": ["美丽", "美术", "十全十美"], "sentence": "公园里的花坛特别美。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOtSf6kB96IgYo7lqHkCYgjAQLngDarMfvXwp4G6prI8RhQbMJrUC_dQkygad6eE87YN9PKOM5vf3R4nhiUr5RmRstYNRHO8T0-N-QOXidM2lr0RxKfXvmt6PnL2qy3UMFh4_yEb9eU2up1_XoVwmo8DQ=w1024-h1024-s-no-gm?authuser=0" },
+			{ "chinese": "丑", "english": "Ugly", "pinyin": "Chǒu", "phrase": ["丑陋", "小丑", "跳梁小丑"], "sentence": "把脸洗干净了，要不然看上去很丑。", "image": "https://lh3.googleusercontent.com/pw/AP1GczOtSf6kB96IgYo7lqHkCYgjAQLngDarMfvXwp4G6prI8RhQbMJrUC_dQkygad6eE87YN9PKOM5vf3R4nhiUr5RmRstYNRHO8T0-N-QOXidM2lr0RxKfXvmt6PnL2qy3UMFh4_yEb9eU2up1_XoVwmo8DQ=w1024-h1024-s-no-gm?authuser=0" }
+		]
+	}
+]
